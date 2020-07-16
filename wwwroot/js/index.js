0ae60bf80a0a49d1d9d5373ac6cc6ad7697a5b8c
Sean Keever <sean@ravenlogistics.com>
2020-07-16 11:44:50 -0400

    finished the jquery module

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,21 +1,35 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    
+    var x = 0;
+    var s = "";
 
 
-console.log("Hello pluralsight");
+    console.log("Hello pluralsight");
 
 
 
-var theForm = $("#theForm");
-theForm.hide();
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = $("#buyButton");
-button.on("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+
 });
 
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-    console.log("You clicked on " + $(this).text());
-});
 
+});
