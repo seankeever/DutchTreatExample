45c18cdba8cc5513ac60acb8fe1407db48257e9e
Sean Keever <sean@ravenlogistics.com>
2020-07-16 15:03:24 -0400

    fixed compile issue with useEndpoints and i dont know what i did different so i am going to commit while it still compiles

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
 
@@ -23,9 +24,29 @@ namespace DutchTreat
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();   //this was for when I was serving index.html now I am using mvc instead so comment this out
+
+            if(env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //TODO: add error page
+            }
+
             app.UseStaticFiles();
             app.UseNodeModules();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute( "Fallback",
+                   "{controller}/{action}/{id?}",
+                   new { controller = "App", action = "Index" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
