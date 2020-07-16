dd9bdef4673892dfa9a9ff114edde8b5fda33b02
Sean Keever <sean@ravenlogistics.com>
2020-07-16 10:39:57 -0400

    Added jQuery functionality

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace DutchTreat
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿var x = 0;
+var s = "";
+
+
+console.log("Hello pluralsight");
+
+
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
+
